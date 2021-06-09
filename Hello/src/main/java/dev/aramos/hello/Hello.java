3f44b16dc718d66aedd4a933c943044116367b98
albertramos.dev@gmail.com <albert@localhost>
2021-06-09 12:35:28 -0700

    Continuing with new print lines

--- Hello/src/main/java/dev/aramos/hello/Hello.java
+++ Hello/src/main/java/dev/aramos/hello/Hello.java
@@ -13,5 +13,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Github.");
         System.out.println("Hello Github.");
+        System.out.println("Hello?");
+        System.out.println("Heeeelloooooo?");
     }
 }
