1b86b9542fa7834b637dc3f817f801de14ded235
albertramos.dev@gmail.com <albert@localhost>
2021-06-11 08:12:28 -0700

    add user-defined input

--- Adder/src/main/java/adder/Adder.java
+++ Adder/src/main/java/adder/Adder.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package adder;
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +12,28 @@ package adder;
  */
 public class Adder {
     public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        
         int sum = 0;
-        int operand1 = 3;
-        int operand2 = 2;
+        int operand1 = 0;
+        int operand2 = 0;
+        
+        String stringOperand1 = "";
+        String stringOperand2 = "";
+        
+       
+        
+        System.out.println("Please enter the first number to be added: ");
+        stringOperand1 = myScanner.nextLine();
+        
+        System.out.println("Please enter the second number to be added: ");
+        stringOperand2 = myScanner.nextLine();
+        
+        operand1 = Integer.parseInt(stringOperand1);
+        operand2 = Integer.parseInt(stringOperand2);
         
         sum = operand1 + operand2;
+        
         System.out.println("Sum is: " + sum);
     }
 }
