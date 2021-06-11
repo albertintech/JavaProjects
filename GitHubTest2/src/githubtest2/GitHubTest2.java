45be97935008544695572172c9d803eb47f044fe
albertramos.dev@gmail.com <albert@localhost>
2021-06-11 05:49:50 -0700

    Initial commit

---
+++ GitHubTest2/src/githubtest2/GitHubTest2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest2;
+
+/**
+ * Created this program using 'Java with Ant'
+ * Used the 'Create with Main Class' box checkmarked
+ * This had the effect of creating the initial public
+ * class 'GitHubTest2' below instead of an empty file
+ * File also faster to build and run than a Maven project
+ * @author albert
+ */
+public class GitHubTest2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Github.");
+    }
+    
+}
