6807dbb192886202c060c572048e5ea8142b2a95
albertramos.dev@gmail.com <albert@localhost>
2021-06-11 05:35:24 -0700

    Initial commit

---
+++ Adder/src/main/java/adder/Adder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adder;
+
+/**
+ *
+ * @author albert
+ */
+public class Adder {
+    public static void main(String[] args){
+        int sum = 0;
+        int operand1 = 3;
+        int operand2 = 2;
+        
+        sum = operand1 + operand2;
+        System.out.println("Sum is: " + sum);
+    }
+}
