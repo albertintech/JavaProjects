7453099f0924f50ed61eb7851643e3a5c6d60613
albertramos.dev@gmail.com <albert@localhost>
2021-06-12 07:11:38 -0700

    version 1.0

--- QuestForTheUserInput/src/questfortheuserinput/QuestForTheUserInput.java
+++ QuestForTheUserInput/src/questfortheuserinput/QuestForTheUserInput.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package questfortheuserinput;
+import java.util.Scanner;
 /**
  *
  * @author albert
@@ -15,6 +16,29 @@ public class QuestForTheUserInput {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner inputReader = new Scanner(System.in);
+        
+        String yourName;
+        String yourQuest;
+        double velocityOfSwallow;
+        
+        System.out.println("What is your name?");
+        yourName = inputReader.nextLine();
+        
+        System.out.println("What is your quest?");
+        yourQuest = inputReader.nextLine();
+        
+        System.out.println("What is the airspeed velocity of an uladen swallow?!");
+        velocityOfSwallow = Double.parseDouble(inputReader.nextLine());
+        System.out.println();
+        
+        System.out.println("How do you know " + velocityOfSwallow +
+                " is correct, " + yourName + "?");
+        System.out.println("You didn't even know if the swallow was " +
+                "African or European!");
+        System.out.println("Maybe skip answering things about birds " +
+                "and instead go " + yourQuest + ".");
+                
     }
     
 }
