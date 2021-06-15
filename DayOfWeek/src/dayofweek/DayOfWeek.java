5f768fc326eee821ef60ebdc10d58b3b01289cfa
Albert Ramos <albertramos.dev@gmail.com>
2021-06-14 17:29:18 -0700

    added switch stmnt

--- DayOfWeek/src/dayofweek/DayOfWeek.java
+++ DayOfWeek/src/dayofweek/DayOfWeek.java
@@ -15,26 +15,53 @@ public class DayOfWeek {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int day = 2;
+        int day = 9;
         String dayName = "";
         
-        if (day == 1) {
-            dayName = "Monday";
-        } else if (day == 2) {
-            dayName = "Tuesday";
-        } else if (day == 3) {
-            dayName = "Wednesday";
-        } else if (day == 4) {
-            dayName = "Thursday";
-        } else if (day == 5) {
-            dayName = "Friday";
-        } else if (day == 6) {
-            dayName = "Saturday";
-        } else if (day == 7) {
-            dayName = "Sunday";
-        } else {
-            dayName = "Oops! The hard-coded value is not between 1 and 7!";
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "an Invalid day";
+                   
         }
+        
+//        if (day == 1) {
+//            dayName = "Monday";
+//        } else if (day == 2) {
+//            dayName = "Tuesday";
+//        } else if (day == 3) {
+//            dayName = "Wednesday";
+//        } else if (day == 4) {
+//            dayName = "Thursday";
+//        } else if (day == 5) {
+//            dayName = "Friday";
+//        } else if (day == 6) {
+//            dayName = "Saturday";
+//        } else if (day == 7) {
+//            dayName = "Sunday";
+//        } else {
+//            dayName = "Oops! The hard-coded value is not between 1 and 7!";
+//        }
         
         System.out.println("This day is " + dayName);
         
