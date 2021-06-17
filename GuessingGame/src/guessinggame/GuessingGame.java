26b897e654abce158e79adf890c9d4af71061d23
Albert Ramos <albertramos.dev@gmail.com>
2021-06-17 13:26:43 -0700

    added more examples

--- GuessingGame/src/guessinggame/GuessingGame.java
+++ GuessingGame/src/guessinggame/GuessingGame.java
@@ -5,7 +5,7 @@
  */
 package guessinggame;
 import java.util.Scanner;
-import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -18,6 +18,26 @@ public class GuessingGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Random rng = new Random();
+        
+        //generate 10 numbers, each between 0 and 9 inclusive
+        for (int i=0; i <= 10; i++)
+        {
+            int randomNumber = rng.nextInt(10) + 1;
+            System.out.println(randomNumber);
+        }
+        
+        //Randomly select a name from an array of names
+        String[] names = new String[] { "Alice", "Bob", "Clarice", "David",
+            "Elizabeth"
+        };
+        int randomIndex = rng.nextInt( names.length );
+        String randomName = names[randomIndex];
+        System.out.println("The winner is: " + randomName + "!");
+        
+        double randomValue = rng.nextDouble();
+        boolean coinIsHeads = randomValue < 0.5;
+        System.out.println("The coin flip is heads: " + coinIsHeads + ".");
     }
     
 }
